79be423b69229cb0203ab42e6f181256f4ce8ae2
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2019-08-13 14:16:53 -0300

    Adding class01 shapes

---
+++ CompGraphics_01_Shapes/src/class01_2019_08_13/Graphics.java
@@ -0,0 +1,32 @@
+package class01_2019_08_13;
+
+//import java.awt.Color;
+//import java.awt.Dimension;
+//import java.awt.Graphics;
+//import javax.swing.JFrame;
+
+/**
+ *
+ * @author wesley
+ */
+public class Graphics {
+    
+//    public Graphics() {
+//        setTitle("Graphics");
+//        setSize(960, 960);
+//        setVisible(true);
+//        setDefaultCloseOperation(EXIT_ON_CLOSE);
+//    }
+//    
+//    public void paint(Graphics g) {
+//        g.setColor(Color.GREEN);
+//        g.drawOval(480, 480, 200, 200);
+//    }
+//    
+//    
+//    public static void main(String[] args) {
+//        Graphics g = new Graphics();
+//        g.paint(null);
+//    }
+    
+}
